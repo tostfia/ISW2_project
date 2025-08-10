70df28c6dccf70b57a6bbe5ef6b501dd4eb2b20a
sofia <tostisofia25@gmail.com>
2025-08-10 16:45:56 +0200

    Piccolo bug risolto

--- src/main/java/org/apache/Main.java
+++ src/main/java/org/apache/Main.java
@@ -7,30 +7,20 @@ import org.apache.logging.CollectLogger;
 import java.util.logging.Logger;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args)  {
         Logger logger= CollectLogger.getInstance().getLogger();
-        if(args.length<1){
-            logger.severe("Uso: java -jar analyzer.jar <file_config> [progetto |--all]");
-            throw new Exception("Error: file di configurazione mancante");
-
-        }
-        String target = args[0]; //Utente specifica il progetto
-        // Se il secondo parametro è --all, allora si analizzano tutti i progetti
-        String projectToAnalyze = (args.length > 1 ? args[1]: "--all");
+        String configFilePath = args[0]; //File json di configurazione
         logger.info("--------------------------------------------ANALISI AVVIATA--------------------------------------------");
         long startTime = System.currentTimeMillis();
 
         try {
             AnalysisController controller = new AnalysisController();
-            controller.startAnalysis(projectToAnalyze);
+            controller.startAnalysis(configFilePath);
         }catch (Exception e){
             logger.severe(e.getMessage());
         }
         long endTime = System.currentTimeMillis();
-        logger.info("--------------------------------------------ANALISI TERMINATA in"+ (endTime-startTime)/1e-9+"secondi--------------------------------------------");
-
-
-
+        logger.info("--------------------------------------------ANALISI TERMINATA in"+ (endTime-startTime)/1000.0+"secondi--------------------------------------------");
 
 
     }
