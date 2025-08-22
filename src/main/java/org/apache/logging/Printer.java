017679cdab47602adf02d6a82cbda66c57279185
sofia <tostisofia25@gmail.com>
2025-08-23 01:24:54 +0200

    Rimosso dei codesmells

--- src/main/java/org/apache/logging/Printer.java
+++ src/main/java/org/apache/logging/Printer.java
@@ -1,34 +1,54 @@
 package org.apache.logging;
 
 
+
+
 public class Printer {
 
+    // Costanti ANSI per i colori
+    public static final String RESET  = "\u001B[0m";
+    public static final String RED    = "\u001B[31m";
+    public static final String GREEN  = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE   = "\u001B[34m";
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_RESET1= "\u001B[0m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    private Printer(){}
-    public static void print(String s){System.out.println(s);}
-    public static void println(String s){System.out.println(s);}
-    public static void printlnBlue(String s){
-        System.out.println(ANSI_BLUE+s+ANSI_RESET1);
-    }
-    public static void printYellow(String s){
-        System.out.println(ANSI_YELLOW+s+ANSI_RESET1);
+    // Costruttore pubblico
+    public Printer() {}
+
+    // Stampa semplice
+    public static void print(String s) {
+        System.out.print(s);
     }
 
-    public static void printBlue(String s){
-        System.out.println(ANSI_BLUE+s+ANSI_RESET1);
-    }
-    public static void printlnGreen(String s){System.out.println(ANSI_GREEN+s+ANSI_RESET1);}
-    public static void printGreen(String s){System.out.println(ANSI_GREEN+s+ANSI_RESET1);}
-    public static void errorPrint(String s){
-        System.out.println(ANSI_RED+s+ANSI_RESET);
+    public static void println(String s) {
+        System.out.println(s);
     }
 
+    // Stampa colorata
+    public static void printlnBlue(String s) {
+        System.out.println(BLUE + s + RESET);
+    }
+
+    public static void printBlue(String s) {
+        System.out.print(BLUE + s + RESET);
+    }
+
+    public static void printlnGreen(String s) {
+        System.out.println(GREEN + s + RESET);
+    }
+
+    public static void printGreen(String s) {
+        System.out.print(GREEN + s + RESET);
+    }
+
+    public static void printYellow(String s) {
+        System.out.print(YELLOW + s + RESET);
+    }
+
+    public static void errorPrint(String s) {
+        System.out.println(RED + s + RESET);
+    }
 }
+
 
 
